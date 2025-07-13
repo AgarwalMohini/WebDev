e98fb06e6d9bab977fab371c47f014a471c8bf21
Mohini Agarwal <160155298+AgarwalMohini@users.noreply.github.com>
2025-07-13 23:32:54 +0530

    Completed Project 1 -> Landing Page

---
+++ Landing Page/script.js
@@ -0,0 +1,7 @@
+// Get the CTA button
+const ctaBtn = document.getElementById('cta-btn');
+
+// Add a click event
+ctaBtn.addEventListener('click', function() {
+  alert('Welcome to TechNova! Your free trial has started.');
+});
